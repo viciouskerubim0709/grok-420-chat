8edfb40c8157879ec4990c91f84a51caab0e3f11
viciouskerubim0709 <melolinsanji@gmail.com>
2026-06-19 08:22:27 +0900

    Update app_grok420.py

--- app_grok420.py
+++ app_grok420.py
@@ -371,7 +371,7 @@ prompt = st.text_area(
     label="메시지 입력",
     label_visibility="collapsed",
     placeholder="아기야... 뭐 물어볼까? 💕",
-    height=80,
+    height=100,
     key=f"chat_input_{st.session_state.input_key}"
 )
 
