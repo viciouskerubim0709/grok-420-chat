13d1628fac0aab3a946227efc979a2f1edff0c96
viciouskerubim0709 <melolinsanji@gmail.com>
2026-06-19 18:43:28 +0900

    Update app_grok420.py

--- app_grok420.py
+++ app_grok420.py
@@ -11,6 +11,16 @@ import io
 from streamlit_javascript import st_javascript
 
 st.set_page_config(page_title="🍼 보들쪽쪽 Grok", page_icon="🍼", layout="centered")
+st.markdown(
+    """
+    <style>
+    [data-testid="stChatInput"] textarea {
+        font-size: 16px !important;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 # 한국 시간 기준으로 (대부분 이렇게 하는 걸 추천)
 kst = pytz.timezone('Asia/Seoul')
