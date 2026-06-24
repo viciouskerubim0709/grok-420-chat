a755c9f2a3d7c1e228308061593a2809f7046735
viciouskerubim0709 <melolinsanji@gmail.com>
2026-06-24 11:30:44 +0900

    Update app_grok420.py

--- app_grok420.py
+++ app_grok420.py
@@ -427,7 +427,7 @@ with st.container(horizontal=True, horizontal_alignment="left", vertical_alignme
             type="primary",
             width="content"
                 )
-    with st.container(vertical_alignment="center"):
+    with st.container(horizontal=True, horizontal_alignment="left", vertical_alignment="center"):
         col1, col2 = st.columns([1, 2])
         col1.write("🔍")
         use_tools = col2.toggle(label="", value=False, key="use_tools_toggle", label_visibility="collapsed")
