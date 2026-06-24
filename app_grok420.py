ad3a075a97e7347831f2e43e2b3a876de13da13e
viciouskerubim0709 <melolinsanji@gmail.com>
2026-06-25 00:59:27 +0900

    Update app_grok420.py

--- app_grok420.py
+++ app_grok420.py
@@ -23,7 +23,7 @@ st.markdown("""
         background: #ffafa3 !important;
     }
     div[data-testid*="Popover"] > div > button,
-    div[data-testid*="file_uploader"]  > div{
+    div[data-testid*="file_uploader"] > div{
         background-color: transparent !important;
         border: 0 !important;
     }
