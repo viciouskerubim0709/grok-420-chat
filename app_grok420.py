940c925ab4be43826d727d7d1c716327b2adc2d2
viciouskerubim0709 <melolinsanji@gmail.com>
2026-06-24 23:11:54 +0900

    Update app_grok420.py

--- app_grok420.py
+++ app_grok420.py
@@ -346,7 +346,7 @@ with st.sidebar:
                 background-color: transparent !important;
                 border: 0 !important;
             }
-           div[data-testid="stPopoverBody"] {
+           div[data-testid="stPopoverContent"] {
                 background-color: #FF7E6B !important;
             }
             </style>
