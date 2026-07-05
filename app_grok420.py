4c217b526fd658b712fcd95abf94dff2f54dd1bb
viciouskerubim0709 <melolinsanji@gmail.com>
2026-07-05 22:41:29 +0900

    Update app_grok420.py

--- app_grok420.py
+++ app_grok420.py
@@ -16,12 +16,10 @@ from st_copy import copy_button
 st.set_page_config(page_title="🍼 보들쪽쪽 Grok", page_icon="🍼", layout="centered")
 st.markdown("""
     <style>
-[data - testid = "stChatMessageContent"] p,
-[data - testid = "stChatMessageContent"] li
-{
-    padding: 0rem !important;
-}
-
+    .main > div {
+        padding-left: 0rem;
+        padding-right: 0rem;
+    }
     
     .stTextArea textarea {
         font-size: 16px !important;
