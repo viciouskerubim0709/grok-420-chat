91d50775b161e0270dcb8bf4ae8fbcb498cae2ed
viciouskerubim0709 <melolinsanji@gmail.com>
2026-07-05 22:12:14 +0900

    Update app_grok420.py

--- app_grok420.py
+++ app_grok420.py
@@ -18,16 +18,8 @@ st.markdown("""
     <style>
     [data-testid="stChatMessageContent"] p,
     [data-testid="stChatMessageContent"] li {
-        font-size: 1rem !important;     /* 본문과 동일한 크기 */
+        font-size: 1.2rem !important;     /* 본문과 동일한 크기 */
         line-height: 1.65;
-    }
-    
-    /* 리스트 들여쓰기 조절 (원하면 숫자 바꿔봐) */
-    [data-testid="stChatMessageContent"] ul,
-    [data-testid="stChatMessageContent"] ol {
-        padding-left: 1.4rem;
-        margin-top: 0.35rem;
-        margin-bottom: 0.6rem;
     }
     
     /* 리스트 아이템 간격 */
