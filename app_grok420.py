b9020fc5e3ac46e764f5e460a589a79b20224330
viciouskerubim0709 <melolinsanji@gmail.com>
2026-07-12 03:14:05 +0900

    Update app_grok420.py

--- app_grok420.py
+++ app_grok420.py
@@ -23,7 +23,7 @@ st.markdown("""
     .st-key-chat_list {
         max-height: 25rem !important;
         overflow-y: scroll !important;
-        padding-right: 0.1rem !important;
+        padding-right: 0.3rem !important;
     }
     div[data-testid="stPopoverBody"],
     div[data-testid*="Popover"] > div:not(:has(> button)){
