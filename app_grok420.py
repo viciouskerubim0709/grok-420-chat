68d161f30ebd66ac3cb116ae98051b5adde16283
viciouskerubim0709 <melolinsanji@gmail.com>
2026-07-11 22:52:01 +0900

    Update app_grok420.py

--- app_grok420.py
+++ app_grok420.py
@@ -28,7 +28,7 @@ st.markdown("""
         padding-bottom: 0rem !important;
     }
     [data-testid="column"] {
-        width: auto !important;
+        width: 27rem !important;
         flex: 1 1 auto !important;
     }
     [data-testid="stHorizontalBlock"] {
