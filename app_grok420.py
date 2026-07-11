bb226825197a263241125b44569cae1ba15794a2
viciouskerubim0709 <melolinsanji@gmail.com>
2026-07-12 00:46:15 +0900

    Update app_grok420.py

--- app_grok420.py
+++ app_grok420.py
@@ -32,7 +32,7 @@ st.markdown("""
     .st-key-chat_list [class*="st-key-chat_item_"] {
         flex: 1 1 auto !important;
         font-size: 15px !important;
-        background: "#FFF8F5" !important;
+        background: "#FFECE5" !important;
         padding-left: 0rem !important;
         border-radius: 10px !important;
     }
