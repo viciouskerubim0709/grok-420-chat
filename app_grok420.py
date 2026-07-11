c000e3fdcec05fe67046a6d6649ecc36465ce78c
viciouskerubim0709 <melolinsanji@gmail.com>
2026-07-12 03:57:33 +0900

    Update app_grok420.py

--- app_grok420.py
+++ app_grok420.py
@@ -343,7 +343,7 @@ with st.sidebar:
     # 대화 목록 + 삭제 버튼
     to_delete = None
     
-    with st.container(key="chat_list", use_container_width=True):
+    with st.container(key="chat_list", width="stretch"):
     
         for chat_id, chat in list(sorted_chats):
             is_current = (chat_id == current)
