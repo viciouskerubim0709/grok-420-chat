0ec1e63e8399775230be0c7e0a3bb77a8243dcf5
viciouskerubim0709 <melolinsanji@gmail.com>
2026-07-12 05:39:36 +0900

    Update app_grok420.py

--- app_grok420.py
+++ app_grok420.py
@@ -21,7 +21,8 @@ st.markdown("""
         max-height: 150px !important;
     }
     .st-key-chat_list {
-        max-width: 100% !important;
+        min-width: 100% !important;
+        flex: 1 1 auto;
         max-height: 24rem !important;
         overflow-x: hidden !important;
         overflow-y: scroll !important;
