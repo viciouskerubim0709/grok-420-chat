5adbf6008e5f2406f0415c5bc627e9d9e5ed407f
viciouskerubim0709 <melolinsanji@gmail.com>
2026-08-10 02:19:04 +0900

    Update app_grok420.py

--- app_grok420.py
+++ app_grok420.py
@@ -66,7 +66,7 @@ st.markdown("""
         border-radius: 10px !important;
         padding: 0.5rem !important;
     }
-    .st-key-chat_list {
+    .st-key-summary {
         color: #FFAFA3 !important;\
         font-weight: bold !important;
     }
